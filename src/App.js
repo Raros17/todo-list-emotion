3343212317488ce929e7dffb1407f8d54f6e3b99
Raros17 <sookmeoyhj@gmail.com>
2024-01-10 13:42:08 +0900

    design: emotion css 레이아웃

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import TodoItem from './TodoItem';
 import {inputState, listState} from './atoms/listState';
 import { useRecoilState } from 'recoil';
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 function App() {
   const [todo, setTodo] = useRecoilState(listState);
@@ -12,18 +14,23 @@ function App() {
   }
 
   const handleAddBtnClick = () => {
-    if(inputValue.trim() !== ''){
-      const newItem = {
-        id: todo.length,
-        isDone: false,
-        content: inputValue,
-      }
-
-      setTodo([...todo, newItem]);
-
-      setInputValue('')
-    }
+    if (inputValue.trim() !== '') {
+      if (!isDuplicateItem()) {
+        const newItem = {
+          id: todo.length,
+          isDone: false,
+          content: inputValue,
+        };
+        setTodo([...todo, newItem]);
+        setInputValue('');
+      } else {
+        alert("동일한 아이템이 있습니다.");
+      }}
   }
+
+  const isDuplicateItem = () => {
+    return todo.some((item) => item.content === inputValue);
+  };
 
   const handleOnKeyPress = (e) =>{
     if(e.key==='Enter'){
@@ -33,17 +40,24 @@ function App() {
 
   return (
     <div className="App">
-      <section style={{backgroundColor:"orange", height:"100px", display:"flex", justifyContent:"center", alignItems:"center", fontSize:"25px", fontWeight:"800"}}>
-        장 봐올 리스트
+      <section css={css({height:"100px", display:"flex", justifyContent:"center", alignItems:"center", fontSize:"29px", fontWeight:"600", borderBottom:"3px solid #ccc"
+      })}>
+        🍒장 봐올 리스트🍎
       </section>
       <section>
         <section>
-          <h2>장바구니 목록에 아이템 담기</h2>
-          <input type="text" placeholder='할일을 입력하세요' style={{height:"30px", borderRadius:"20px", marginRight:"10px"}} value={inputValue}  onKeyDown={handleOnKeyPress} onChange={handleInputChange}></input>
-          <button onClick={handleAddBtnClick} >추가하기</button>
+          <h2 css={css({fontWeight:"800", fontSize:"25px", textAlign:"start", paddingLeft:"200px", marginTop: "20px", marginBottom:"20px"})}>목록에 아이템 담기</h2>
+          <input type="text" placeholder='살 목록을 입력하세요' 
+          css={css({height:"35px", borderRadius:"20px", marginRight:"10px", width: "40%", paddingLeft:"15px"})}  
+          value={inputValue}  onKeyDown={handleOnKeyPress} onChange={handleInputChange}></input>
+          <button css={css({height: "35px", borderRadius:"30px", background:"#777", color: "#fff", cursor:"pointer"})}  
+          onClick={handleAddBtnClick} >추가하기</button>
           </section>
-        <section>
-          {todo.map(item => ( <TodoItem key={item.id} item={item}/>))}
+        <section css={css({display:"flex", justifyContent:"center"})}>
+          <div css={css({width:"45%", marginTop:"50px", border:"1px solid #ccc", borderRadius:"10px"})}>
+            {todo.map(item => ( <TodoItem key={item.id} item={item}/>
+            ))}
+            </div>
         </section>
         {/* 성공할 때마다 참 잘했어요 하면서 하트가 막 떠오르게 못함? */}
       </section>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { listState } from "./atoms/listState";
 import { useRecoilState } from "recoil";
-
-
+import { MdDelete } from "react-icons/md";
+import { FaPenToSquare } from "react-icons/fa6";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 function TodoItem({item}) {
   const [todo, setTodo] = useRecoilState(listState);
@@ -13,12 +15,12 @@ function TodoItem({item}) {
   }
 
   return (
-    <div style={{display:"flex", justifyContent:"center"}}>
-        <div><input type="checkbox"/></div>
-        <span>{item.content}</span>
+    <div css={css({display:"flex", justifyContent:"left", margin:"20px"})}>
+        <div><input type="checkbox" css={css({zoom:"1.4", cursor:"pointer"})}/></div>
+        <span css={css({fontSize:"20px", fontWeight:"700", marginLeft:"10px", marginRight:"10px"})}>{item.content}</span>
         <div>
-          <button>수정</button>
-          <button onClick={()=> handleDeleteItemClick(item.id)}>삭제</button>
+          <button css={css({cursor:"pointer"})}><FaPenToSquare /></button>          
+          <button css={css({cursor:"pointer"})} onClick={()=> handleDeleteItemClick(item.id)}><MdDelete/></button>
         </div>
     </div>
   );
